ea03fd76ba412dd92fba7e9ba2a444403a7e3e96
Keith Murimi <keith.murimi@student.moringaschool.com>
2024-07-28 21:57:06 +0300

    First commit

--- main.js
+++ main.js
@@ -1,8 +1,39 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const hearts = document.querySelectorAll('.like-glyph');
+
+  function toggleHeart(event) {
+    const heart = event.target;
+
+    mimicServerCall()
+      .then(() => {
+        if (heart.innerText === EMPTY_HEART) {
+          heart.innerText = FULL_HEART;
+          heart.classList.add('activated-heart');
+        } else {
+          heart.innerText = EMPTY_HEART;
+          heart.classList.remove('activated-heart');
+        }
+      })
+      .catch((error) => {
+        const errorModal = document.getElementById('modal');
+        errorModal.innerText = error;
+        errorModal.classList.remove('hidden');
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  }
+
+  // Adding click event listeners to each heart
+  hearts.forEach((heart) => {
+    heart.addEventListener('click', toggleHeart);
+  });
+});
 
 
 
